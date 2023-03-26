0f2d5a7132298e55ca2bbec96199cc3e298f61b0
nahid-dev <nahidv232@gmail.com>
2023-03-27 00:36:19 +0600

    products db loaded

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
 
       <div>
         <a href="/shop">Shop</a>
-        <a href="/order">Order</a>
+        <a href="/order">Orders</a>
         <a href="/inventory">Inventory</a>
         <a href="/login">Login</a>
       </div>
