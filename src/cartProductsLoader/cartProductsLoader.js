e5269e2d77b9d7814f32e284939775f32c51f247
nahid-dev <nahidv232@gmail.com>
2023-04-07 02:56:00 +0600

    impiment order page

---
+++ src/cartProductsLoader/cartProductsLoader.js
@@ -0,0 +1,21 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
+const cartProductsLoader = async () => {
+  const loadedProducts = await fetch("products.json");
+  const products = await loadedProducts.json();
+
+  const storedCart = getShoppingCart();
+  const saveCart = [];
+  for (const id in storedCart) {
+    const addedProduct = products.find((pd) => pd.id === id);
+    if (addedProduct) {
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+      saveCart.push(addedProduct);
+    }
+  }
+  console.log(saveCart);
+  return saveCart;
+};
+
+export default cartProductsLoader;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from "./component/Shop/Shop";
+import Home from "./component/Layout/Home";
+import Orders from "./component/Orders/Orders";
+import Inventory from "./component/Inventory/Inventory";
+import Login from "./component/Login/Login";
+import cartProductsLoader from "./cartProductsLoader/cartProductsLoader";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/",
+        element: <Shop></Shop>,
+      },
+      {
+        path: "orders",
+        element: <Orders></Orders>,
+        loader: cartProductsLoader,
+      },
+      {
+        path: "inventory",
+        element: <Inventory></Inventory>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
